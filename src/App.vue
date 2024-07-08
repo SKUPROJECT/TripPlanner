a47ae6007abab8a725676d8fc1b2fef7c2df707b
sun8183 <4sunskyhyun@gmail.com>
2024-07-08 20:48:01 +0900

    Vue 개발 초기 세팅

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld class="yang-title" msg="Welcome to Your Vue.js App"/>
 </div>
 </template>
 
